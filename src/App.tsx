636083322bae7eb41e572255ff66568f393e482c
Rizky Tegar Pratama <mail.rizkytegar@gmail.com>
2023-10-27 10:17:05 +0700

    🚀 Fixed CSS on mobile: Device responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { DateTime } from "luxon"; 
+import { DateTime } from "luxon";
 /**
  * Renders a digital clock component that displays the current time in the selected time zone.
  *
@@ -34,39 +34,47 @@ function App() {
   };
 
   return (
-    <main className="bg-primary-focus grid h-screen place-items-center px-6 py-24 sm:py-32 lg:px-8">
-      <div className="card w-96 bg-base-100 shadow-xl">
+    <main className="bg-primary-focus grid h-screen place-items-center px-6 py-24 w-full lg:px-8">
+      <div className="card w-full md:w-96 bg-base-100 shadow-xl">
         <div className="card-body">
-          <p className="text-base font-semibold text-indigo-600">
+        <p className="text-base font-semibold text-indigo-600">
             {loading
               ? "Loading..."
               : jam.toFormat("cccc, dd LLLL yyyy", { locale: "id" })}
           </p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-            {loading ? "Loading..." : jam.toFormat("HH:mm:ss")}
-          </h1>
+          <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+            <div className="flex flex-col p-2 bg-white rounded-box text-gray-90">
+              <span className="font-mono text-5xl">
+              {loading ? ".." : jam.toFormat("HH")}
+              </span>
+              hours
+            </div>
+            <div className="flex flex-col p-2 bg-white rounded-box text-gray-90">
+              <span className="font-mono text-5xl">
+              {loading ? ".." : jam.toFormat("mm")}
+              </span>
+              min
+            </div>
+            <div className="flex flex-col p-2 bg-white rounded-box text-gray-90">
+              <span className="font-mono text-5xl">
+              {loading ? ".." : jam.toFormat("ss")}
+              </span>
+              sec
+            </div>
+          </div>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <select
               onChange={handleZoneChange}
               value={zone}
               className="select bg-secondary text-white select-bordered w-full max-w-xs"
             >
-              <option
-                className="bg-white text-primary"
-                value="Asia/Jakarta"
-              >
+              <option className="bg-white text-primary" value="Asia/Jakarta">
                 WIB (Jakarta)
               </option>
-              <option
-                className="bg-white text-primary"
-                value="Asia/Makassar"
-              >
+              <option className="bg-white text-primary" value="Asia/Makassar">
                 WITA (Makassar)
               </option>
-              <option
-                className="bg-white text-primary"
-                value="Asia/Jayapura"
-              >
+              <option className="bg-white text-primary" value="Asia/Jayapura">
                 WIT (Jayapura)
               </option>
             </select>
