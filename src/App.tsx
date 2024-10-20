7a95f79ad783f87dc5e8607a3bbded7afcdbd77c
__dwky__ <mail.rizkytegar@gmail.com>
2024-10-20 13:33:12 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 // App.tsx
-import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import VeoLayouts from './Layouts/VeoLayouts';
